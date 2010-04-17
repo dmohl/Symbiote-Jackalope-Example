c1e3cd071faba51b644b75e8adef3d7514202fe3
Dan Mohl <dmohl@yahoo.com>
2010-04-17 09:21:54 -0500

    Created

--- SymbioteDemo/Program.fs
+++ SymbioteDemo/Program.fs
@@ -4,6 +4,8 @@ open System
 open Symbiote.Core
 open Symbiote.Relax
 open Symbiote.Daemon
+open Symbiote.Jackalope
+open Symbiote.Jackalope.Impl
 
 type PersonCouchDocument =
     val id : string
@@ -17,17 +19,28 @@ type PersonCouchDocument =
         member x.Address
             with get () = x.address
 
+type HandleMessage() =
+    interface IMessageHandler<PersonCouchDocument> with
+        member x.Process (message:PersonCouchDocument, messageDelivery:IMessageDelivery) =
+            Console.WriteLine("Processing message for person {0}.", message.name)
+            messageDelivery.Acknowledge()
+
 type DemoService = 
     val couchServer : ICouchServer
-    new(couchServer) = {couchServer = couchServer}
+    val bus : IBus
+    new(couchServer, bus) as this = 
+        {couchServer = couchServer; bus = bus} then this.Initialize()
+    member public x.Initialize () =
+        x.bus.AddEndPoint(fun x -> 
+            x.Exchange("SymbioteDemo", ExchangeType.fanout).QueueName("SymbioteDemo")
+                .Durable().PersistentDelivery() |> ignore)
     interface IDaemon with
         member x.Start () =
             do Console.WriteLine("The service has started")
+            x.bus.Subscribe("SymbioteDemo", null)
             let document = new PersonCouchDocument("123456", "John Doe", "123 Main")
             x.couchServer.Repository.Save(document)
-            do Console.WriteLine(
-                "The document with name {0} and address {1} was saved successfully", 
-                document.Name, document.Address)
+            x.bus.Send("SymbioteDemo", document)
             let documentRetrieved = 
                 x.couchServer.Repository.Get<PersonCouchDocument>(document.DocumentId);
             do Console.WriteLine(
@@ -39,9 +52,10 @@ type DemoService =
 
 do Assimilate
     .Core()
-    .Daemon<DemoService>(fun x -> (x.Name("FSharpDemoService")
+    .Daemon(fun x -> (x.Name("FSharpDemoService")
                                     .DisplayName("An FSharp Demo Service")
                                     .Description("An FSharp Demo Service")
                                     .Arguments([||]) |> ignore))
     .Relax(fun x -> x.Server("localhost") |> ignore) 
-    .RunDaemon<DemoService>() 
+    .Jackalope(fun x -> x.AddServer(fun s -> s.Address("localhost").AMQP08() |> ignore) |> ignore)
+    .RunDaemon() 
